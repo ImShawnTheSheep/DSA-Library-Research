bacaadf7fb21d9263039d4b5d1985d5685952834
Shawn Michael Jumawan <shawnmichaeljumawan@gmail.com>
2022-12-10 21:25:51 +0800

    Integrated MongoDB

--- Python/database-sim.py
+++ Python/database-sim.py
@@ -1,2 +1,16 @@
-import mysql.connector
+import pymongo
+from pymongo import MongoClient
 
+cluster = MongoClient("mongodb+srv://shawnjumawan:VM6YYjXFTDmoAd9a@shawncluster.ywjvagz.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["sampledatabase"]
+collection = db["studentinfo"]
+
+# print(db.list_collection_names())
+
+collection.insert_one({
+    '_id': 'student1',
+    'studentname': 'Shawn Jumawan',
+    'studentnumber': '2021-00363-CM-0',
+    'loglocation': 'Rothlener Bldg',
+    'time': 1030
+})
--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,7 @@
 
+
+
+
 # This is the main function
 if __name__ == "__main__":
 
