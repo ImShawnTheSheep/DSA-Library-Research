9312935d3f8f656b5fc7783c248e946ba7a29357
Shawn Michael Jumawan <shawnmichaeljumawan@gmail.com>
2022-12-10 19:46:44 +0800

    Add mySQL Connector for Python

--- Python/database-sim.py
+++ Python/database-sim.py
@@ -1,12 +1,2 @@
-student1 = {
-    'student-number': '2021-00363-CM-0',
-    'student-name': 'Shawn Michael Jumawan',
-}
+import mysql.connector
 
-# print(student1['student-name'])
-
-if student1['student-name'] == 'Shawn Michael Jumawan':
-    print('Hi ' + student1['student-name'])
-    print('Your student number is ' + student1['student-number'])
-else:
-    print('Student unknown')
