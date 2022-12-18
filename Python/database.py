53b43d7010c3a0de013a320ae7aae1962631b886
Shawn Michael Jumawan <shawnmichaeljumawan@gmail.com>
2022-12-18 16:39:39 +0800

    Modified DATABASE module

--- Python/database.py
+++ Python/database.py
@@ -1,7 +1,8 @@
 import pymongo
 from pymongo import MongoClient
 
-studData = MongoClient("mongodb+srv://shawnjumawan:VM6YYjXFTDmoAd9a@shawncluster.ywjvagz.mongodb.net/?retryWrites=true&w=majority")
+studData = MongoClient(
+    "mongodb+srv://shawnjumawan:VM6YYjXFTDmoAd9a@shawncluster.ywjvagz.mongodb.net/?retryWrites=true&w=majority")
 # studData = MongoClient("mongodb://localhost:27017/")
 db = studData["AMSDatabase"]
 collect = db["Student Info"]
